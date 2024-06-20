6726b89fd9e7062f389eed94bc19774b018a93b3
srssubudhi <145208726+srssubudhi@users.noreply.github.com>
2024-06-20 07:57:43 +0530

    first commit

---
+++ public/client.js
@@ -0,0 +1,58 @@
+const socket = io()
+let name;
+let textarea = document.querySelector('#textarea')
+let send= document.querySelector('#send')
+let messageArea = document.querySelector('.message__area')
+do {
+    name = prompt('Please enter your name: ')
+} while(!name)
+
+send.addEventListener('click', (e) => {
+    e.preventDefault()
+    // console.log('clicked')
+    let message = textarea.value
+    if(message) {
+        sendMessage(message)
+    }
+    else {
+        alert('Please enter a message')
+    }    
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+    // Append 
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    // Send to server 
+    socket.emit('message', msg)
+
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+// Recieve messages 
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
